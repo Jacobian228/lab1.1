2130d5507bea1fc04f3aaf15c303634481805edc
Jacobian228 <128458132+Jacobian228@users.noreply.github.com>
2023-03-26 18:01:10 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-
-function App() {
+class App extends Component {
+    
+    
+    render(){
   return (
     <div className="App">
 <h1>Тест ПДД</h1>
@@ -13,69 +16,69 @@ function App() {
 
 <div class="grid-container">
     <div class="grid-item">
-        <img alt="A screenshot showing CSS Quick Edit" src="screenshots/quick-edit.png" />
+        <img alt="A screenshot showing CSS Quick Edit" src="screenshots/111.png" />
         <a>
-            <label>А</label>
-            <input type="checkbox" id="a1" name="f1" onClick={window['onlyOne(this)']} value='1'/>
-            <label>Б</label>
-            <input type="checkbox" id="a2" name="f1" onClick={window['onlyOne(this)']} value='0'/>
-            <label>В</label>
-            <input type="checkbox" id="a3" name="f1" onClick={window['onlyOne(this)']} value='0'/>
+            <label>1)</label>
+            <input type="radio" id="a1" name="f1" value='0'/>
+            <label>2)</label>
+            <input type="radio" id="a2" name="f1" value='0'/>
+            <label>3)</label>
+            <input type="radio" id="a3" name="f1" value='1'/>
         </a>
-    </div>
+    </div>  
    <div class="grid-item">
-        <img alt="A screenshot showing CSS Quick Edit" src="screenshots/quick-edit.png" />
+        <img alt="A screenshot showing CSS Quick Edit" src="screenshots/222.png" />
         <a>
-            <label>А</label>
-            <input type="checkbox" id="a1" name="f2" onClick={window['onlyOne(this)']} value='0'/>
-            <label>Б</label>
-            <input type="checkbox" id="a2" name="f2" onClick={window['onlyOne(this)']} value='1'/>
-            <label>В</label>
-            <input type="checkbox" id="a3" name="f2" onClick={window['onlyOne(this)']} value='0'/>
+            <label>1)</label>
+            <input type="radio" id="a1" name="f2" value='1'/>
+            <label>2)</label>
+            <input type="radio" id="a2" name="f2" value='0'/>
+            <label>3)</label>
+            <input type="radio" id="a3" name="f2" value='0'/>
         </a>
     </div>
     <div class="grid-item">
-        <img alt="A screenshot showing CSS Quick Edit" src="screenshots/quick-edit.png" />
+        <img alt="A screenshot showing CSS Quick Edit" src="screenshots/333.png" />
         <a>
-            <label>А</label>
-            <input type="checkbox" id="a1" name="f3" onClick={window['onlyOne(this)']} value='0'/>
-            <label>Б</label>
-            <input type="checkbox" id="a2" name="f3" onClick={window['onlyOne(this)']} value='0'/>
-            <label>В</label>
-            <input type="checkbox" id="a3" name="f3" onClick={window['onlyOne(this)']} value='1'/>
+            <label>1)</label>
+            <input type="radio" id="a1" name="f3" value='0'/>
+            <label>2)</label>
+            <input type="radio" id="a2" name="f3" value='0'/>
+            <label>3)</label>
+            <input type="radio" id="a3" name="f3" value='1'/>
         </a>
     </div>
     <div class="grid-item">
-        <img alt="A screenshot showing CSS Quick Edit" src="screenshots/quick-edit.png" />
+        <img alt="A screenshot showing CSS Quick Edit" src="screenshots/444.png" />
         <a>
-            <label>А</label>
-            <input type="checkbox" id="a1" name="f4" onClick={window['onlyOne(this)']} value='1'/>
-            <label>Б</label>
-            <input type="checkbox" id="a2" name="f4" onClick={window['onlyOne(this)']} value='0'/>
-            <label>В</label>
-            <input type="checkbox" id="a3" name="f4" onClick={window['onlyOne(this)']} value='0'/>
+            <label>1)</label>
+            <input type="radio" id="a1" name="f4" value='0'/>
+            <label>2)</label>
+            <input type="radio" id="a2" name="f4" value='0'/>
+            <label>3)</label>
+            <input type="radio" id="a3" name="f4" value='1'/>
         </a>
     </div>
     <div class="grid-item">
-        <img alt="A screenshot showing CSS Quick Edit" src="screenshots/quick-edit.png" />
+        <img alt="A screenshot showing CSS Quick Edit" src="screenshots/555.png" />
         <a>
-            <label>А</label>
-            <input type="checkbox" id="a1" name="f5" onClick={window['onlyOne(this)']} value='0'/>
-            <label>Б</label>
-            <input type="checkbox" id="a2" name="f5" onClick={window['onlyOne(this)']} value='1'/>
-            <label>В</label>
-            <input type="checkbox" id="a3" name="f5" onClick={window['onlyOne(this)']} value='0'/>
+            <label>1)</label>
+            <input type="radio" id="a1" name="f5" value='1'/>
+            <label>2)</label>
+            <input type="radio" id="a2" name="f5" value='0'/>
+            <label>3)</label>
+            <input type="radio" id="a3" name="f5" value='0'/>
         </a>
     </div>
     <div class="grid-item">
-        <img alt="A screenshot showing CSS Quick Edit" src="screenshots/quick-edit.png" />
+        <img alt="A screenshot showing CSS Quick Edit" src="screenshots/666.png" />
         <a>
-            <label>А</label>
-            <input type="checkbox" id="a1" name="f6" onClick={window['onlyOne(this)']} value='0'/>
-            <label>Б</label>
-            <input type="checkbox" id="a2" name="f6" onClick={window['onlyOne(this)']} value='0'/>
-            <label>В</label>
-            <input type="checkbox" id="a3" name="f6" onClick={window['onlyOne(this)']} value='1'/>
+            <label>1)</label>
+            <input type="radio" id="a1" name="f6" value='0'/>
+            <label>2)</label>
+            <input type="radio" id="a2" name="f6" value='1'/>
+            <label>3)</label>
+            <input type="radio" id="a3" name="f6" value='0'/>
         </a>
     </div>
 </div>
@@ -85,7 +88,7 @@ function App() {
 
 <p id="result">Результат:</p>
     </div>
-  );
+  );}
 }
 
 
